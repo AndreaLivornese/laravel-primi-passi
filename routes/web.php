c03b99df7f4744cd9c6dc6704fc8e9f87ce2f290
andrealivornese <andrealivornese@gmail.com>
2024-04-05 15:58:48 +0200

    Titolo passato dinamicanìmente tramite al route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Home');
+
+    $word = "Hello World!";
+
+    return view('Home', compact("word"));
 });
